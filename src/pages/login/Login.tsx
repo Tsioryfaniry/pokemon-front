776f1bcad7c03062a2c0d5e6bae5835fbfb4101a
Tsioryfaniry <andrianarivonyfaniry@gmail.com>
2024-03-13 17:04:51 +0300

    Style input

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,83 +1,31 @@
 // import { Form } from "react-router-dom";
 // import Heading from "@/components/heading/Heading";
+import Input from "../../components/input/Input";
 import style from "./login.module.scss";
 // function action(){
 
 // }
 
 export default function Login() {
+  const items = [
+    { type: "text", label: "your email", placeholder: "your email" },
+    { type: "text", label: "your email", placeholder: "phone number" },
+    { type: "text", label: "Password", placeholder: "password" },
+    { type: "text", label: "Confirm password", placeholder: "password" },
+  ];
+  const Items = items.map((item) => (
+    <Input
+      key={item.label}
+      type={item.type}
+      placeholder={item.placeholder}
+      label={item.label}
+    />
+  ));
   return (
     <div className="max-w-sm m-auto pt-4">
       {/* <Heading>Login</Heading> */}
       <form className="max-w-sm mx-auto pt-4" action="/login">
-        <div>
-          <label
-            htmlFor="email"
-            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-          >
-            Your email
-          </label>
-          <input
-            type="email"
-            name="email"
-            id="email"
-            className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            placeholder="name@company.com"
-            required
-          />
-        </div>
-        <div>
-          <label
-            htmlFor="password"
-            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-          >
-            Password
-          </label>
-          <input
-            type="password"
-            name="password"
-            id="password"
-            placeholder="••••••••"
-            className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            required
-          />
-        </div>
-        <div className="flex items-center justify-between mt-2">
-          <div className="flex items-start mt-2">
-            <div className="flex items-center h-5">
-              <input
-                id="remember"
-                aria-describedby="remember"
-                type="checkbox"
-                className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
-                required
-              />
-            </div>
-            <div className="ml-3 text-sm ">
-              <label
-                htmlFor="remember"
-                className="text-gray-500 dark:text-gray-300"
-              >
-                Remember me
-              </label>
-            </div>
-          </div>
-        </div>
-        <button
-          type="submit"
-          className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
-        >
-          Sign in
-        </button>
-        <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-          Don’t have an account yet?{" "}
-          <a
-            href="#"
-            className="font-medium text-primary-600 hover:underline dark:text-primary-500"
-          >
-            Sign up
-          </a>
-        </p>
+        {Items}
       </form>
     </div>
   );
