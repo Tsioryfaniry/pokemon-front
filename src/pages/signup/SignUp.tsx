eb06f8f65349506c3b73adb50c480d38a89e31be
Tsioryfaniry <andrianarivonyfaniry@gmail.com>
2024-03-13 17:00:29 +0300

    Add logon

---
+++ src/pages/signup/SignUp.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+// import { Form, Link } from "react-router-dom";
+import Heading from "../../components/heading/Heading";
+import Input from "../../components/input/Input";
+import style from "./signup.module.scss";
+
+export default function SignUp() {
+  const items = [
+    { type: "text", label: "your email", placeholder: "your email" },
+    { type: "text", label: "your email", placeholder: "phone number" },
+    { type: "text", label: "Password", placeholder: "password" },
+    { type: "text", label: "Confirm password", placeholder: "password" },
+  ];
+  const Items = items.map((item) => (
+    <Input
+      key={item.label}
+      type={item.type}
+      placeholder={item.placeholder}
+      label={item.label}
+    />
+  ));
+  return (
+    <>
+      <Heading component="h1" variant="h1" className="text-center">
+        Sign up
+      </Heading>
+
+      <form action="/signup" method="post" className="max-w-sm mx-auto pt-4">
+        {Items}
+        <button type="submit" className={style.button}>
+          Register new account
+        </button>
+      </form>
+    </>
+  );
+}
